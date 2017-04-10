b1ae3322295e257ad86f4045a08595eb7b14af18
cuttlas <dani.pare@gmail.com>
2017-04-10 16:23:39 +0200

    setupDB: del records on tearDown

--- src/api/setupDb.js
+++ src/api/setupDb.js
@@ -2,14 +2,8 @@ const knex = require("./knex");
 const testData = require("./testData");
 
 exports.populateDB = () => {
-  return knex("article_newsletter")
-    .del()
-    .then(() => knex("newsletters").del())
-    .then(() => knex("articles").del())
-    .then(() => knex("for_later").del())
-    .then(() => knex("favourites").del())
-    .then(() => knex("users").del())
-    .then(() => knex("articles").insert(testData.articles))
+  return knex("articles")
+    .insert(testData.articles)
     .then(() => knex("newsletters").insert(testData.newsletters))
     .then(() => knex("article_newsletter").insert(testData.articleNewsletter))
     .then(() => knex("users").insert(testData.users))
@@ -17,4 +11,13 @@ exports.populateDB = () => {
     .then(() => knex("favourites").insert(testData.favourites));
 };
 
-exports.destroyDB = () => knex.destroy();
+exports.destroyDB = () => {
+  return knex("article_newsletter")
+    .del()
+    .then(() => knex("newsletters").del())
+    .then(() => knex("articles").del())
+    .then(() => knex("for_later").del())
+    .then(() => knex("favourites").del())
+    .then(() => knex("users").del())
+    .then(() => knex.destroy());
+};
