6337fd506169cd7ce4a2275cf34854fa79e8c1dd
cuttlas <dani.pare@gmail.com>
2017-05-16 23:40:27 +0200

    sort publisher by positioin

--- scrapers/publisher.js
+++ scrapers/publisher.js
@@ -8,7 +8,7 @@ const getArticleToPublish = async newsletter => {
     .join("article_newsletter", "articles.id", "article_newsletter.article_id")
     .where("article_newsletter.newsletter_id", newsletter.id)
     .whereNull("published")
-    .orderBy("id", "DESC");
+    .orderBy("position");
 
   return articles[0];
 };
--- scrapers/reset.sh
+++ scrapers/reset.sh
@@ -2,6 +2,6 @@ knex seed:run --knexfile='../db/knexfile.js'
 node ./scrapers/runScraper javascriptWeekly 318 333
 node ./scrapers/runScraper reactjsNewsletter 57 72
 node ./scrapers/runScraper ponyFoo 46 61
-node ./scrapers/runScraper frontendFocus 163 179
+node ./scrapers/runScraper frontendFocus 273 289
 node ./scrapers/runScraper cssWeekly 249 263
 node ./scrapers/runPublisher 200
